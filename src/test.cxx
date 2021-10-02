9cc136bbd70c16b6b90e6ab1a736b50d1d425bd9
Nick Tyler <nicholas.s.tyler.4@gmail.com>
2021-10-02 11:27:10 -0700

    Update test.cxx

--- src/test.cxx
+++ src/test.cxx
@@ -1,35 +1,44 @@
 #include <iostream>
 #include <vector>
 #include <omp.h>
-//#include "wrapper.hpp"
 
-void spmv(std::vector<float> a, std::vector<float> v, std::vector<float> x, int nrows) {
+#ifdef TARGET_GPU
+#include "wrapper.hpp"
+#endif
 
-    float val=0.0f;
+std::vector<float> spmv(std::vector<float> a, std::vector<float> v, std::vector<float> x, int nrows) {
+
     int num_threads = omp_get_num_procs();
 
     #pragma omp parallel for num_threads(num_threads)
     for(int z=0; z<nrows*nrows; ++z ){
-        #pragma acc parallel loop 
+        float val=0.0f;
+        #pragma acc kernel
+        {
+        #pragma acc parallel loop
         for(int i=0; i<nrows; ++i ){
             for(int n=0; n<nrows; ++n){
                 val += a[n*nrows+i]*v[i*nrows+n] + x[i*nrows+n];
             }
-
-            x[i] = val;
+        }
+        // #pragma acc wait
+        x[z%nrows] = val;
         }
     }
-
+    return x;
 } 
 
 int main(int argc, char const *argv[]) {
-    //print_cuda_properties();
+
+    #ifdef TARGET_GPU
+    print_cuda_properties();
+    #endif
+
     const int nrows = 1000;
 
     std::vector<float> a(nrows*nrows), v(nrows*nrows), x(nrows*nrows);
 
-
-    #pragma acc parallel loop vector_length(128)
+    #pragma omp parallel for collapse(2)
     for(int i=0; i<nrows; ++i ){
         for(int j=0; j<nrows; ++j ){
             a[i*nrows+j] = static_cast<float>(random());
